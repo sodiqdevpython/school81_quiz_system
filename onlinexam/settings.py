c826d266741585440492d7382259acf914fac68f
Sodiq <sodiq7898@gmail.com>
2023-03-10 23:27:34 +0500

    added some css styles with js

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')       
+# STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')       
 
 
 
